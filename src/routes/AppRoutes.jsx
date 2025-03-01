c6f1504f8d1d2237ee3c3d3d9e23b7226fb278cd
Muhammad Raihan Resa <muhraihanresa243@gmail.com>
2025-03-01 23:02:27 +0800

    create footer

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,11 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "../component/Navbar";
+import Footer from "../component/footer";
 
 const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path="/" element={<Navbar />} />
+            <Route path="/" element={
+                <>
+                    <Navbar />
+                    <Footer />
+                </>
+            } />
         </Routes>
     )
 }
