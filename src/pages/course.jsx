69ac5ee33c681c86ef0f568527c5324fd64bad2c
MuhRezkyPasuloi <resky.pasuloi@gmail.com>
2025-03-03 22:32:14 +0800

    course page (finish)

--- src/pages/course.jsx
+++ src/pages/course.jsx
@@ -50,5 +50,68 @@ const CourseCard = () => {
   );
 };
 
+const DifficultySection = () => {
+  return (
+    <section className='mb-12'>
+        <div className="flex justify-between items-center mb-6">
+            <div className="w-1/5">
+                <h2 className="text-xl font-semibold">Tingkat Kesulitan</h2>
+            </div>
+            <div className="flex w-3/5 justify-center items-center space-x-4">
+                <div className="flex-grow border-t-2 border-red-600"></div>
+                <button className="bg-red-500 text-white px-4 py-2 rounded-full">Pemula</button>
+                <button className="bg-gray-200 text-gray-500 px-4 py-2 rounded-full">Menengah</button>
+                <button className="bg-gray-200 text-gray-500 px-4 py-2 rounded-full">Lanjutan</button>
+                <div className="flex-grow border-t-2 border-red-600"></div>
+            </div>
+            <div className="w-1/5"></div>
+        </div>
+      
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <CourseCard />
+        <CourseCard />
+        <CourseCard />
+      </div>
+      <div className="flex justify-center mt-6">
+        <button className="bg-gray-200 text-gray-500 rounded-full p-2 mx-2">
+          <FaChevronLeft />
+        </button>
+        <button className="bg-red-500 text-white rounded-full p-2 mx-2">
+          <FaChevronRight />
+        </button>
+      </div>
+    </section>
+  );
+};
 
+const CategorySection = () => {
+    return (
+      <section>
+        <div className="flex justify-between items-center mb-6">
+            <div className="w-1/5">
+                <h2 className="text-xl font-semibold">Tingkat Kesulitan</h2>
+            </div>
+            <div className="flex w-3/5 justify-center items-center space-x-4">
+                <div className="flex-grow border-t-2 border-red-600"></div>
+                <button className="bg-red-500 text-white px-4 py-2 rounded-full flex">Pidana <FaChevronDown className='pl-1 my-1'/></button>
+                <div className="flex-grow border-t-2 border-red-600"></div>
+            </div>
+            <div className="w-1/5"></div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+        </div>
+        <div className="flex justify-center mt-6">
+          <button className="bg-gray-200 text-gray-500 rounded-full p-2 mx-2">
+            <FaChevronLeft />
+          </button>
+          <button className="bg-red-500 text-white rounded-full p-2 mx-2">
+            <FaChevronRight />
+          </button>
+        </div>
+      </section>
+    );
+  };
 export default Course;
